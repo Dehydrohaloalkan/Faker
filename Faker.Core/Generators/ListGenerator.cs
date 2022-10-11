2f3fcc1abec55d76e4d7563d567953232c14f97a
alexb <alexbondar03@gmail.com>
2022-10-11 11:02:59 +0300

    Add List generator

---
+++ Faker.Core/Generators/ListGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Reflection;
+using Faker.Core.Interfaces;
+
+namespace Faker.Core.Generators;
+
+public class ListGenerator : IGenerator
+{
+    public object Generate(Type type, GeneratorContext context)
+    {
+        var length = context.Random.Next(3, 5);
+        var list = (IList)Activator.CreateInstance(type);
+        if (list != null)
+            for (var i = 0; i < length; i++)
+                list.Add(context.Faker.Create(type.GetGenericArguments()[0]));
+        return list;
+    }
+    public bool CanGenerate(Type type)
+    {
+        return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>);
+    }
+}
